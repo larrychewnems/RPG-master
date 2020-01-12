69505bdb80171ab9298fb503956129827b6e239c
Larry Chew <Larry.Chew@nems.org>
2020-01-11 21:42:10 -0800

    Added Camera Zoom Botton

---
+++ RPG Project/Assets/Scripts/Button/ZoomBtn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomBtn : MonoBehaviour
+{
+    enum zoomLevels {zoom1=33, zoom2=40, zoom3=48}
+    public Sprite[] sprite;
+    [SerializeField] zoomLevels zoomLevel = zoomLevels.zoom1;
+
+    private int zoomLevelsCounts = System.Enum.GetValues(typeof(zoomLevels)).Length;
+
+    private void Awake() {
+        SetZoomLevel();
+    }
+
+    public void SetZoomLevel()
+    {
+        int index = System.Array.IndexOf(System.Enum.GetValues(zoomLevel.GetType()), zoomLevel);
+        //Debug.Log("enum: " + (int)zoomLevel + ", count: " + zoomLevelsCounts.ToString() + ", index: " + index);
+
+        if (index >= zoomLevelsCounts-1)
+        {
+            index=0;
+            GetComponent<Image>().sprite = sprite[0];
+        }else {
+            index++;
+            if(index >= zoomLevelsCounts - 1)
+            {
+                GetComponent<Image>().sprite = sprite[1];
+            }
+        }
+
+        zoomLevel = (zoomLevels) (System.Enum.GetValues(zoomLevel.GetType())).GetValue(index);
+        Camera.main.fieldOfView = (int) zoomLevel;
+    }
+}
--- RPG Project/Assets/Scripts/Control/Player/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private float mouseKeyDownSec = 0f;            //how long is the mouse key being held down.
     private Mover mover;
+    private bool clicked = false;
 
     [SerializeField] float stopDistance = 0.5f;
     [SerializeField] float tabDownSpeed = 0.5f;    //mouse key tab down speed in sec.
@@ -21,13 +22,19 @@ public class PlayerController : MonoBehaviour
         {
             mouseKeyDownSec += Time.deltaTime;
             //Debug.Log("mouse held time: " + mouseKeyDownSec);
-            MoveToCursor();
+            //Debug.Log(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject());
+            if(!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            {
+                clicked=true;
+                MoveToCursor();
+            }  
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (mouseKeyDownSec > tabDownSpeed)
+            if (mouseKeyDownSec > tabDownSpeed && clicked)
             {
+                clicked=false;
                 mover.StopMove();
             }
             mouseKeyDownSec = 0f;
